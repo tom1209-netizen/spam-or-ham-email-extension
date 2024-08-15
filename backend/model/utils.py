3e5d69c79f5d701791ef786758308a47e5f5b088
Lee Khan <lephuckhang0@gmail.com>
2024-08-15 14:40:47 +0700

    initial code

---
+++ backend/model/utils.py
@@ -0,0 +1,37 @@
+import nltk
+
+
+def lowercase(text):
+    return text.lower()
+
+
+def punctuation_removal(text):
+    translator = str.maketrans('', '', string.punctuation)
+
+    return text.translate(translator)
+
+
+def tokenize(text):
+    return nltk.word_tokenize(text)
+
+
+def remove_stopwords(tokens):
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    return [token for token in tokens if token not in stop_words]
+
+
+def stemming(tokens):
+    stemmer = nltk.PorterStemmer()
+
+    return [stemmer.stem(token) for token in tokens]
+
+
+def preprocess_text(text):
+    text = lowercase(text)
+    text = punctuation_removal(text)
+    tokens = tokenize(text)
+    tokens = remove_stopwords(tokens)
+    tokens = stemming(tokens)
+
+    return tokens
