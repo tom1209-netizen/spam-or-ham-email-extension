33bcd3c7ddf9baa3a024d741ad42cbe864245f52
Lee Khan <lephuckhang0@gmail.com>
2024-08-16 16:17:00 +0700

    Fix: add guinicorn and update app to match render document

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from model.spam_detector import SpamDetector
 from utils import load_dictionary, load_label_encoder, predict
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,5 @@ def predict_route():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
