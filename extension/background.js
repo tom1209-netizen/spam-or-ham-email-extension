9a1c8e988e60b37621f4f6d266c067dd874c2fbe
Lee Khan <lephuckhang0@gmail.com>
2024-08-16 17:03:11 +0700

    Fix: put requirements.txt into the right place

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "getEmailContent" }, (response) => {
             if (response) {
-                document.getElementById("content").textContent = response.content;
+                const emailContent = response.content;
+                document.getElementById("content").textContent = emailContent;
+
+                const url = 'https://spam-or-ham-email-extension.onrender.com/predict';
+
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ text: emailContent }),  // Send the email content as part of the POST request
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    document.getElementById("result").textContent = data.prediction;
+                })
+
             } else {
                 document.getElementById("content").textContent = "Failed to retrieve email content.";
             }
